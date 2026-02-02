6fb7a84fb7b89778ab470ece82590fc9ff719630
NelsonObita <nelsonobita@protonmail.com>
2026-02-01 22:32:29 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace COMP1030_W26_Lab_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
