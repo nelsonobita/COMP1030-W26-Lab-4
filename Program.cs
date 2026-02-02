9096a7b4e959e87c98201f1dfd970e14bda03129
NelsonObita <nelsonobita@protonmail.com>
2026-02-01 22:50:46 -0500

    Implemented weekday wake-up logic (6am threshold)

--- Program.cs
+++ Program.cs
@@ -15,7 +15,33 @@ int hour = Int32.Parse(hourString);
 
 // STEP 3: Create an 'if' condition to check whether it is the weekend
 if (day == "Saturday" || day == "Sunday")
-
+{
+    // STEP 5a: It is either Saturday or Sunday, so now we need an if/else condition to see whether it is time to wake up or not
+    if (hour >= 9)
+    {
+        // STEP 5b: It is 9am or later
+        Console.WriteLine("It is the weekend and it is time to get up and enjoy the day.");
+    }
+    else
+    {
+        // STEP 5c: It is not yet 9am
+        Console.WriteLine("It is not yet time to get out of bed.");
+    }
+}
+else
+{
+    // STEP 6a: We need to create a condition that checks to see whether it is 6am or later
+    if (hour >= 6)
+    {
+        // STEP 6b: It is indeed 6am or later
+        Console.WriteLine("It is time to wake up and get ready for the work day.");
+    }
+    else
+    {
+        // STEP 6c: It is not yet 6am
+        Console.WriteLine("It is not yet time to get out of bed.");
+    }
+}
 
 
 
